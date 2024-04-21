3277e27e623bc278670d63c0fdd8a1058fedeb13
Jeni <106121800+Jeni1336@users.noreply.github.com>
2024-04-21 14:28:46 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import styles from './AppStyle';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text></Text>
+    </View>
+  );
+}
+
